0cae60058369d5d4f83b8ec9feaf2adaccf82828
klaircodes <mariel.sarcia2004@gmail.com>
2025-10-07 14:57:47 -0400

    added router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Start from './Start.jsx';
-
-
 import GirlStart from './GirlStart.jsx';
+import GirlPath1 from './GirlPath1.jsx';
+import GirlPath2 from './GirlPath2.jsx';
+import GirlPath1a from './GirlPath1a.jsx';
+import MainContent from './MainContent.jsx';
+import MusicPlayer from './MusicPlayer.jsx';
 
 export default function App() {
-  const [opened, setOpened] = useState(false);
-  return opened ? (
-    <GirlStart />
-  ) : (
-    <Start onOpen={() => setOpened(true)} />
+  return (
+    <Router>
+      <MusicPlayer autoPlay={true} />
+      <Routes>
+        <Route path="/" element={<Start />} />
+        <Route path="/girlstart" element={<GirlStart />} />
+        <Route path="/girlpath1" element={<GirlPath1 />} />
+        <Route path="/girlpath2" element={<GirlPath2 />} />
+        <Route path="/girlpath1a" element={<GirlPath1a />} />
+        <Route path="/maincontent" element={<MainContent />} />
+      </Routes>
+    </Router>
   );
 }
